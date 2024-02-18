da143aed7ff8164d895c6308fc624e50e13822b5
AtharvaPC <baodhankaratharva@gmail.com>
2024-02-18 10:57:07 +0530

    added AboutUs  Section

--- script.js
+++ script.js
@@ -12,3 +12,32 @@ navBtns.forEach((btn) => {
     });
   });
 
+// var navAnimate = gsap.timeline();
+
+// navAnimate.from(".nav-left", {
+//   y:-100
+// })
+
+var preloader = gsap.timeline();
+
+
+preloader.from(".preloader h1 span", {
+  letterSpacing: "60px",
+  duration: 1,
+  opacity: 0,
+  stagger:.15
+})
+
+preloader.to(".preloader h1 span", {
+  letterSpacing: "60px",
+  duration: 1,
+  opacity: 0,
+  stagger:.15
+})
+preloader.to(".preloader", {
+  opacity: 0,
+})
+preloader.to(".preloader", {
+  left:"-100%"
+})
+
