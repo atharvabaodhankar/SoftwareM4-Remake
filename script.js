ff50d52dc92b28c40eba9a00a136f6f8a5026f0b
AtharvaPC <baodhankaratharva@gmail.com>
2024-02-19 09:50:36 +0530

    added AboutUs  Section

--- script.js
+++ script.js
@@ -19,25 +19,43 @@ navBtns.forEach((btn) => {
 // })
 
 var preloader = gsap.timeline();
+var mm = gsap.matchMedia();
 
+mm.add("(max-width:900px)", () => {
 
-preloader.from(".preloader h1 span", {
-  letterSpacing: "60px",
-  duration: 1,
-  opacity: 0,
-  stagger:.15
+  preloader.from(".preloader h1", {
+    // fontSize: 500,
+    x:"+=200",
+    duration: 1.5,
+  },'a')
+  preloader.to(".preloader h1", {
+    x: "-=200",
+    delay:1,
+    duration: 1.5,
+  },'a') 
+})
+mm.add("(min-width:900px)", () => { 
+  preloader.from(".preloader h1 span", {
+    letterSpacing: "60px",
+    duration: 1,
+    opacity: 0,
+    stagger:.15
+  })
+  
+  preloader.to(".preloader h1 span", {
+    letterSpacing: "60px",
+    duration: 1,
+    opacity: 0,
+    stagger:.15
+  })
+ 
 })
 
-preloader.to(".preloader h1 span", {
-  letterSpacing: "60px",
-  duration: 1,
-  opacity: 0,
-  stagger:.15
-})
 preloader.to(".preloader", {
   opacity: 0,
 })
 preloader.to(".preloader", {
   left:"-100%"
 })
+
 
